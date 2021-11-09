cc11b12d16f8f3a2763b533c25010cceb4a56b10
Carlos <Carlos@ceduardopg>
2021-11-09 18:01:31 -0200

    Teste.

---
+++ src/main/java/br/edu/ifsul/lpoo/csgo2021_02/ClasseTeste.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ifsul.lpoo.csgo2021_02;
+
+/**
+ *
+ * @author Carlos
+ */
+public class ClasseTeste {
+    
+}
